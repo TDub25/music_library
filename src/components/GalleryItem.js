ebab42918d8b32915a449515662ae549b4990d25
Travis Storie <storie25@outlook.com>
2022-09-10 12:06:48 -0700

    Minor changes again

--- src/components/GalleryItem.js
+++ src/components/GalleryItem.js
@@ -32,7 +32,7 @@ function GalleryItem(props) {
 
     const detailView = () => {
         return (
-            <div>
+            <div style={detailStyle}>
                 <h2>{props.item.trackName}</h2>
                 <h3>{props.item.collectionName}</h3>
                 <h4>{props.item.primaryGenreName}</h4>
@@ -50,6 +50,5 @@ function GalleryItem(props) {
 
         </div>
     )
-
 }
 export default GalleryItem
